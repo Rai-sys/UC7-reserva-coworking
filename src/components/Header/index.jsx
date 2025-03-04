713c902181bb90f4b95e13705ba9ce7229a82807
Rai-sys <railaneserafim484@gmail.com>
2025-03-04 02:46:02 -0300

    add: adicionado o header e o home para finalizar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import style from "./Header.module.css";
+import icon from "../../assets/icon/icon.png"
 
 function Header() {
     return (
-        <header className={style.header}></header>
+        <header className={style.header}>
+            <div class="logo">
+                <img src={icon} alt="algo"/>
+            </div>
+
+                <nav>
+                    <ul>
+                        <li> Início </li>
+                        <li> Sobre </li>
+                        <li> Reserva </li>
+                        <li> Contato </li>
+                    </ul>
+                </nav>
+            <div className={style.button_login}>
+                <h3> Login </h3>
+            </div>
+        </header>
     )
 }
+
+export default Header;
